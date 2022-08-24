1460be0173c62a7ab9c22857bd1636dfcef02642
tekno <teknogyan123@gmail.com>
2022-08-25 03:41:32 +0530

    Implemented Volley Library to send API request instead of HttpURLConnection Class

--- app/src/main/java/com/example/cowinvaccineavailibilitycheck/Getdata.java
+++ app/src/main/java/com/example/cowinvaccineavailibilitycheck/Getdata.java
@@ -1,11 +1,17 @@
 package com.example.cowinvaccineavailibilitycheck;
 
+import android.app.DownloadManager;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -16,7 +22,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 /*
-* This class extends AsyncTask to
+* This class extends AsyncTask to send a request using Volley Library
 *
 */
 
@@ -29,71 +35,53 @@ public class Getdata extends AsyncTask<String, Void, String> {
 
     @Override
     protected String doInBackground(String... strings) {
-//        RequestQueue queue = Volley.newRequestQueue(mainActivity.getApplicationContext());
 
+        RequestQueue queue = Volley.newRequestQueue(mainActivity.getApplicationContext());
 
-        String result = "";
-        URL url;
-        HttpURLConnection urlConnection = null;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, mainActivity.url, null, new Response.Listener<JSONObject>() {
 
-        try {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
 
-            url = new URL(strings[0]);
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = urlConnection.getInputStream();
-            InputStreamReader reader = new InputStreamReader(in);
-            int data = reader.read();
+                            JSONArray arr = response.getJSONArray("sessions");
 
-            while (data != -1) {
+                            for (int i = 0; i < arr.length(); i++) {
 
-                char current = (char) data;
-                result += current;
-                data = reader.read();
+                                JSONObject jsonPart = arr.getJSONObject(i);
 
-            }
-            return result;
+                                mainActivity.locationName.add(i, jsonPart.getString("name") + " has "
+                                        + jsonPart.getString("available_capacity")
+                                        + " doses available");
+                                Log.i("available locations", Integer.toString(arr.length()));
+                                Log.i("loc", Integer.toString(i) + " " + mainActivity.locationName.get(i));
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "failed";
-        }
-    }
+                            }
+                            if (mainActivity.locationName.isEmpty()) {
 
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+                                Toast.makeText(mainActivity
+                                        , "No doses available at this location on this date. Try some other date or location."
+                                        , Toast.LENGTH_SHORT).show();
 
-        try {
+                            }
 
-            JSONObject jsonObject = new JSONObject(s);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        mainActivity.adapter = new ArrayAdapter<String>(mainActivity.getApplicationContext(), android.R.layout.simple_list_item_1, mainActivity.locationName);
+                        mainActivity.availability.setAdapter(mainActivity.adapter);
+                    }
+                }, new Response.ErrorListener() {
 
-            String vacInfo = jsonObject.getString("sessions");
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(mainActivity, "Error Occured", Toast.LENGTH_SHORT).show();
 
-            JSONArray arr = new JSONArray(vacInfo);
+                    }
+                });
 
-            for (int i = 0; i < arr.length(); i++) {
-
-                JSONObject jsonPart = arr.getJSONObject(i);
-
-                mainActivity.locationName.add(i, jsonPart.getString("name") + " has "
-                        + jsonPart.getString("available_capacity")
-                        + " doses available");
-                Log.i("available locations", Integer.toString(arr.length()));
-                Log.i("loc", Integer.toString(i) + " " + mainActivity.locationName.get(i));
-
-            }
-            if (mainActivity.locationName.isEmpty()) {
-
-                Toast.makeText(mainActivity
-                        , "No doses available at this location on this date. Try some other date or location."
-                        , Toast.LENGTH_SHORT).show();
-
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        mainActivity.adapter = new ArrayAdapter<String>(mainActivity.getApplicationContext(), android.R.layout.simple_list_item_1, mainActivity.locationName);
-        mainActivity.availability.setAdapter(mainActivity.adapter);
+        queue.add(jsonObjectRequest);
+    return null;
     }
 }
