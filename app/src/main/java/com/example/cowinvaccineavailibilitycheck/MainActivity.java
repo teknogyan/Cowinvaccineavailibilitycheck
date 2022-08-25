292f24349134f7d390ce3d287a939b9e0ca5ba98
tekno <teknogyan123@gmail.com>
2022-08-25 18:17:41 +0530

    Replaced Async

--- app/src/main/java/com/example/cowinvaccineavailibilitycheck/MainActivity.java
+++ app/src/main/java/com/example/cowinvaccineavailibilitycheck/MainActivity.java
@@ -13,8 +13,21 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
     DatePickerDialog picker;
     ArrayAdapter<String> adapter;
     String url;
-
     public void checkAvail (View view) {
-
         locationName.clear();
+        // hide the keyboard when pressed the button.
         try {
             InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
@@ -43,9 +55,60 @@ public class MainActivity extends AppCompatActivity {
                 + pinCodeByUser + "&date=" + dateByUser;
 
         Log.i("url",url);
+        // Using Executor to run a background thread and request data from the API
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
 
-        Getdata getdata = new Getdata(this);
-        getdata.execute(url);
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+
+                                    JSONArray arr = response.getJSONArray("sessions");
+
+                                    for (int i = 0; i < arr.length(); i++) {
+
+                                        JSONObject jsonPart = arr.getJSONObject(i);
+
+                                        locationName.add(i, jsonPart.getString("name") + " has "
+                                                + jsonPart.getString("available_capacity")
+                                                + " doses available");
+                                        Log.i("available locations", Integer.toString(arr.length()));
+                                        Log.i("loc", Integer.toString(i) + " " + locationName.get(i));
+
+                                    }
+                                    if (locationName.isEmpty()) {
+
+                                        Toast.makeText(getApplicationContext()
+                                                , "No doses available at this location on this date. Try some other date or location."
+                                                , Toast.LENGTH_SHORT).show();
+
+                                    }
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, locationName);
+                                availability.setAdapter(adapter);
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(getApplicationContext(), "Error Occured", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+
+                queue.add(jsonObjectRequest);
+
+            }
+        });
 
     }
 
@@ -60,10 +123,12 @@ public class MainActivity extends AppCompatActivity {
         date.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Get current date
                 final Calendar calendar = Calendar.getInstance();
                 int year = calendar.get(Calendar.YEAR);
                 int month = calendar.get(Calendar.MONTH);
                 int day = calendar.get(Calendar.DAY_OF_MONTH);
+
                 picker = new DatePickerDialog(MainActivity.this
                         , new DatePickerDialog.OnDateSetListener() {
                     @Override
