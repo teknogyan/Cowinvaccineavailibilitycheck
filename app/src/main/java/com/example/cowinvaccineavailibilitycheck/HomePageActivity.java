8352b3551faf48ccd3ef9145207e9313294e8e6e
tekno <teknogyan123@gmail.com>
2022-08-30 04:29:14 +0530

    added one more activity

--- app/src/main/java/com/example/cowinvaccineavailibilitycheck/HomePageActivity.java
+++ app/src/main/java/com/example/cowinvaccineavailibilitycheck/HomePageActivity.java
@@ -2,13 +2,75 @@ package com.example.cowinvaccineavailibilitycheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.concurrent.RunnableScheduledFuture;
 
 public class HomePageActivity extends AppCompatActivity {
+    Button searchByPin;
+    Button searchByDistrict;
+    EditText et_pinCode;
+    EditText et_state;
+    EditText et_district;
+    TextView tv_date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        searchByPin = findViewById(R.id.btn_searchByPin);
+        searchByDistrict = findViewById(R.id.btn_searchByDistrict);
+        et_pinCode = findViewById(R.id.et_pinCode);
+        et_state = findViewById(R.id.et_state);
+        et_district = findViewById(R.id.et_district);
+        tv_date = findViewById(R.id.et_date);
+        View view = this.getCurrentFocus();
+
+        searchByPin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pinIntent = new Intent();
+                pinIntent.putExtra("pinCode", et_pinCode.getText().toString());
+                pinIntent.putExtra("date", tv_date.getText().toString());
+                pinIntent.setClass(getApplicationContext(),SearchByPinActivity.class);
+                startActivity(pinIntent);
+            }
+        });
+        tv_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get current date
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog picker = new DatePickerDialog(HomePageActivity.this
+                        , new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        String datePicked = day + "-" + (month + 1) + "-" + year;
+                        tv_date.setText(datePicked);
+                        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/cowinvaccineavailibilitycheck/SearchByPinActivity.java
+++ app/src/main/java/com/example/cowinvaccineavailibilitycheck/SearchByPinActivity.java
@@ -1,13 +1,9 @@
 package com.example.cowinvaccineavailibilitycheck;
 
-import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -28,28 +24,34 @@ import java.util.Calendar;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchByPinActivity extends AppCompatActivity {
 
-    EditText pinCode;
-    EditText date;
     ListView availability;
-    ArrayList<String> locationName = new ArrayList<String>();
-    DatePickerDialog picker;
+    ArrayList<String> locationName = new ArrayList<>();
+    String pinCodeByUser;
+    String dateByUser;
     ArrayAdapter<String> adapter;
     String url;
-    public void checkAvail (View view) {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_pin);
+        Bundle extras = getIntent().getExtras();
+
+        availability = findViewById(R.id.lv_availability);
+        pinCodeByUser = extras.getString("pinCode");
+        dateByUser = extras.getString("date");
+
+        findViewById(R.id.btn_goBack).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
         locationName.clear();
-        // hide the keyboard when pressed the button.
-        try {
-            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-        } catch (Exception e) {
-            Toast.makeText(this, "Error Occured", Toast.LENGTH_SHORT).show();
-        }
-
-
-        String pinCodeByUser = pinCode.getText().toString();
-        String dateByUser = date.getText().toString();
         url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="
                 + pinCodeByUser + "&date=" + dateByUser;
 
@@ -106,38 +108,6 @@ public class MainActivity extends AppCompatActivity {
 
                 queue.add(jsonObjectRequest);
 
-            }
-        });
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        pinCode = findViewById(R.id.et_PinCode);
-        date = findViewById(R.id.et_ViewDate);
-        availability = findViewById(R.id.lv_availability);
-        date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Get current date
-                final Calendar calendar = Calendar.getInstance();
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                picker = new DatePickerDialog(MainActivity.this
-                        , new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-                        String datePicked = day + "-" + (month + 1) + "-" + year;
-                        date.setText(datePicked);
-
-                    }
-                },year, month, day);
-                picker.show();
             }
         });
 
