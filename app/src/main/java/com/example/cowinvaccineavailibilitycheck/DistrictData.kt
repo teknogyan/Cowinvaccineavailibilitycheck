2d4ddd785e6be50c6e12376ed4d3eec5300474ec
tekno <teknogyan123@gmail.com>
2022-09-08 12:16:56 +0530

    added a little bit of Kotlin

---
+++ app/src/main/java/com/example/cowinvaccineavailibilitycheck/DistrictData.kt
@@ -0,0 +1,4 @@
+package com.example.cowinvaccineavailibilitycheck
+
+// Class to hold data for both doses by district
+class DistrictData(val centreName: String, val dose1: String, val dose2: String, val vaccineName: String)
